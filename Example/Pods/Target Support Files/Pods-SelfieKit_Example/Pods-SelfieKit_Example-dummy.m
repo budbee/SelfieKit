3a8d31e54a6e3f283077bfb782417ea9df499cc6
Axel Möller <axel.moller@budbee.com>
2016-09-24 18:35:22 +0200

    .gitignore

--- Example/Pods/Target Support Files/Pods-SelfieKit_Example/Pods-SelfieKit_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SelfieKit_Example : NSObject
-@end
-@implementation PodsDummy_Pods_SelfieKit_Example
-@end
--- Example/Pods/Target Support Files/SelfieKit/SelfieKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SelfieKit : NSObject
-@end
-@implementation PodsDummy_SelfieKit
-@end
